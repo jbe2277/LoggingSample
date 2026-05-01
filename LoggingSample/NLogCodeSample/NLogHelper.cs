c82d6dcbf8f05b3fce08018fa05ba703de98ddfa
jbe2277 <jbe2277@users.noreply.github.com>
2026-05-01 23:09:21 +0200

    Simplify the logger setup

--- LoggingSample/NLogCodeSample/NLogHelper.cs
+++ LoggingSample/NLogCodeSample/NLogHelper.cs
@@ -32,23 +32,8 @@ internal static class NLogHelper
     {
         return LoggerFactory.Create(builder =>
         {
-            var configuration = LogManager.Configuration ?? throw new InvalidOperationException("LogManager.Configuration is null");
-            foreach (var x in configuration.LoggingRules)
-            {
-                builder.AddFilter(x.LoggerNamePattern, x.Levels.Min()?.ToMSLogLevel() ?? Microsoft.Extensions.Logging.LogLevel.None);
-            }
-            builder.AddNLog();
+            builder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+            builder.AddNLog(new NLogProviderOptions { RemoveLoggerFactoryFilter = true });
         });
-    }
-
-    public static Microsoft.Extensions.Logging.LogLevel ToMSLogLevel(this LogLevel level)
-    {
-        if (level == LogLevel.Trace) return Microsoft.Extensions.Logging.LogLevel.Trace;
-        if (level == LogLevel.Debug) return Microsoft.Extensions.Logging.LogLevel.Debug;
-        if (level == LogLevel.Info) return Microsoft.Extensions.Logging.LogLevel.Information;
-        if (level == LogLevel.Warn) return Microsoft.Extensions.Logging.LogLevel.Warning;
-        if (level == LogLevel.Error) return Microsoft.Extensions.Logging.LogLevel.Error;
-        if (level == LogLevel.Fatal) return Microsoft.Extensions.Logging.LogLevel.Critical;
-        return Microsoft.Extensions.Logging.LogLevel.None;
     }
 }
